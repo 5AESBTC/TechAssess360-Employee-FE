b65f96fbc17b1e2babaeded9095b7b77fdb25b26
DTung2002 <ddtung10.02@gmail.com>
2024-09-10 08:17:06 +0700

    header/footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  </head>
   <div id="app">
     <HomePage />
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+import HomePage from './components/KetQuaDanhGia.vue';
 
 export default {
   name: 'App',
@@ -17,11 +23,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
